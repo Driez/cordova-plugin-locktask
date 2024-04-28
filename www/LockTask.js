3413a178f89d1a1138a4f99b3becebf746a27136
Michael Graf <mike@glitch.tech>
2024-04-28 04:23:37 +0200

    add js side to immersive mode

--- www/LockTask.js
+++ www/LockTask.js
@@ -1,12 +1,12 @@
 module.exports = {
-  startLockTask: function (successCallback, errorCallback, adminClassName, whitelist) {
+  startLockTask: function (successCallback, errorCallback, adminClassName, whitelist, immersive) {
     if (adminClassName == null) {
       adminClassName = '';
     }
     if (!whitelist) {
     whitelist = [];
     }
-    cordova.exec(successCallback, errorCallback, "LockTask", "startLockTask", [adminClassName, whitelist]);
+    cordova.exec(successCallback, errorCallback, "LockTask", "startLockTask", [adminClassName, whitelist, immersive]);
   },
   stopLockTask: function (successCallback, errorCallback) {
     cordova.exec(successCallback, errorCallback, "LockTask", "stopLockTask", ['', []]);
